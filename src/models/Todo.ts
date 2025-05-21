60e1b60ed1164890d4e878bfcd4964502e9139ec
Yasowant <yasowant1998@gmail.com>
2025-05-22 01:51:05 +0530

    add project group

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -1,11 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-
-const todoSchema = new mongoose.Schema({
+const todoSchema = new mongoose.Schema(
+  {
+    projectGroup: { type: String, required: true },
     title: { type: String, required: true },
     description: { type: String },
     completed: { type: Boolean, default: false },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-}, { timestamps: true })
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-export const Todo = mongoose.model('Todo', todoSchema)
+export const Todo = mongoose.model('Todo', todoSchema);
